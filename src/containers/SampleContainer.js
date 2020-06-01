3a651c2b5bab41fd0b22342a5091dbbf4ae732d2
znzmektm2 <tingcber2@naver.com>
2020-06-01 23:26:39 +0900

    add: 리덕스 미들웨어를 통한 비동기 작업 관리(applyMiddleware, createLogger, ReduxThunk, axios)

---
+++ src/containers/SampleContainer.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { getPost, getUsers } from '../modules/sample';
+import Sample from './../components/Sample';
+
+const { useEffect } = React;
+const SampleContainer = ({
+  getPost,
+  getUsers,
+  post,
+  users,
+  loadingPost,
+  loadingUsers
+}) => {
+  // 클래스 형태 컴포넌트였다면componentDidMount
+  useEffect(() => {
+    // getPost(1);
+    // getUsers(1);
+    const fn = async () => {
+      try {
+        await getPost(1);
+        await getUsers(1);
+      } catch (e) {
+        console.log(e); // 에러 조회
+      }
+    };
+    fn();
+  }, [getPost, getUsers]);
+  return (
+    <Sample
+      post={post}
+      users={users}
+      loadingPost={loadingPost}
+      loadingUsers={loadingUsers}
+    />
+  );
+};
+
+export default connect(
+  ({ sample, loading }) => ({
+    post: sample.post,
+    users: sample.users,
+    // loadingPost: sample.loading.GET_POST,
+    // loadingUsers: sample.loading.GET_USERS
+    loadingPost: loading['sample/GET_POST'],
+    loadingUsers: loading['sample/GET_USERS']
+  }),
+  {
+    getPost,
+    getUsers
+  }
+)(SampleContainer);
